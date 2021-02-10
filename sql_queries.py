86e5deedf168ebb63a4179c3a1f7188bf3625f16
Clement <dapsen59@gmail.com>
2021-02-10 03:00:21 -0600

    Add files via upload

--- sql_queries.py
+++ sql_queries.py
@@ -29,12 +29,12 @@ songplay_table_insert = """INSERT INTO songplay (songplay_id, start_time, user_i
 
 user_table_insert = """INSERT INTO users ( user_id, first_name, last_name, gender, level) VALUES( %s, %s, %s, %s, %s) ON CONFLICT(user_id) DO NOTHING; """
 
-song_table_insert = """INSERT INTO songs (song_id, title, artist_id, year, duration)  VALUES( %, %, %, %, %); """
+song_table_insert = """INSERT INTO songs (song_id, title, artist_id, year, duration)  VALUES( %, %, %, %, %) ON CONFLICT(song_id, artist_id) DO NOTHING ; """
 
-artist_table_insert = """INSERT INTO artists(artist_id, name, location, latitude, longitude)  VALUES( %, %, %, %, %); """
+artist_table_insert = """INSERT INTO artists(artist_id, name, location, latitude, longitude)  VALUES( %, %, %, %, %)ON CONFLICT(artist_id) DO NOTHING; """
 
 
-time_table_insert = """INSERT INTO time (start_time, hour, day, week, month, year, weekday)  VALUES( %s, %s, %s, %s, %s, %s, %s); """
+time_table_insert = """INSERT INTO time (start_time, hour, day, week, month, year, weekday)  VALUES( %s, %s, %s, %s, %s, %s, %s)CONFLICT(start_time) DO NOTHING; """
 
 #print(songplay)
 
